bafd4ea6fc29fb4c7099384a0627d452423391fb
papajin <papajini@gmail.com>
2018-05-08 22:26:38 +0300

    Initial setup.

---
+++ classes/Kohana/Sweph.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ihor
+ * Date: 08.05.2018
+ * Time: 22:24
+ */
+
+class Kohana_Sweph {
+
+}
---
+++ classes/Sweph.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ihor
+ * Date: 08.05.2018
+ * Time: 22:22
+ */
+
+class Sweph extends Kohana_Sweph {}
